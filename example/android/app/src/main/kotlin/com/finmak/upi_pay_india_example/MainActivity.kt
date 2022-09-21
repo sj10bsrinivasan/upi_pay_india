9988be0886fa8145ac1392c3bdc79b1b62e9776d
srinivasan <sj10bsrinivasan@live.in>
2022-09-21 19:49:38 +0530

    main

---
+++ example/android/app/src/main/kotlin/com/finmak/upi_pay_india_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.finmak.upi_pay_india_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/upi_pay_india.dart
@@ -0,0 +1,9 @@
+export 'package:upi_pay_india/src/api.dart' show UpiPay;
+export 'package:upi_pay_india/src/applications.dart' show UpiApplication;
+export 'package:upi_pay_india/src/response.dart'
+    show UpiTransactionResponse, UpiTransactionStatus;
+export 'package:upi_pay_india/src/meta.dart' show ApplicationMeta;
+export 'package:upi_pay_india/src/discovery.dart'
+    show
+    UpiApplicationDiscoveryAppStatusType,
+    UpiApplicationDiscoveryAppPaymentType;
